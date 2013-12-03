558499783511a0c04ca57827dd035126d940082d
Simo Sorce <simo@redhat.com>
2013-12-03 12:47:05 -0500

    Initial module
    Signed-off-by: Simo Sorce <simo@redhat.com>

---
+++ src/ipsilon.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2013  Simo Sorce <simo@redhat.com>
+#
+# see file 'COPYING' for use and warranty information
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+sys.stdout = sys.stderr
+
+import atexit
+import threading
+import cherrypy
+
+class Root(object):
+
+    @cherrypy.expose
+    def index(self):
+        return '/'
+
+if __name__ == "__main__":
+    cherrypy.quickstart(Root())
+
+else:
+    cherrypy.config.update({'environment': 'embedded'})
+
+    if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
+        cherrypy.engine.start(blocking=False)
+        atexit.register(cherrypy.engine.stop)
+
+    application = cherrypy.Application(Root(), script_name=None, config=None)
