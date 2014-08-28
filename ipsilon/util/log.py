9c13685d58b242c865fd203fd7b4a239fbf07ac1
Simo Sorce <simo@redhat.com>
2014-08-28 14:25:15 -0400

    Add error log facility to Log utility
    Also improve debug errors by adding the originating function
    
    Signed-off-by: Simo Sorce <simo@redhat.com>
    Reviewed-by: Patrick Uiterwijk <puiterwijk@redhat.com>

--- ipsilon/util/log.py
+++ ipsilon/util/log.py
@@ -5,16 +5,21 @@
 # See the file named COPYING for the project license
 
 import cherrypy
+import inspect
 
 
 class Log(object):
 
     def debug(self, fact):
         if cherrypy.config.get('debug', False):
-            cherrypy.log(fact)
+            s = inspect.stack()
+            cherrypy.log('DEBUG(%s): %s' % (s[1][3], fact))
 
     # for compatibility with existing code
     _debug = debug
 
     def log(self, fact):
         cherrypy.log(fact)
+
+    def error(self, fact):
+        cherrypy.log.error('ERROR: %s' % fact)
