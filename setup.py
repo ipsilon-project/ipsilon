9f546df3b596380ec577fcb379f33f370673415a
Patrick Uiterwijk <puiterwijk@redhat.com>
2014-12-11 22:33:44 +0100

    Update version and maintainer info
    Signed-off-by: Patrick Uiterwijk <puiterwijk@redhat.com>
    Reviewed-by: Simo Sorce <simo@redhat.com>

--- setup.py
+++ setup.py
@@ -1,21 +1,6 @@
 #!/usr/bin/python
 #
-# Copyright (C) 2013  Simo Sorce <simo@redhat.com>
-#
-# see file 'COPYING' for use and warranty information
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright (C) 2014  Ipsilon project Contributors, for licensee see COPYING
 
 from distutils.core import setup
 from glob import glob
@@ -24,10 +9,10 @@ DATA = 'share/ipsilon/'
 
 setup(
     name = 'ipsilon',
-    version = '0.2.6',
+    version = '0.3.0',
     license = 'GPLv3+',
-    maintainer = 'Simo Sorce',
-    maintainer_email = 'simo@redhat.com',
+    maintainer = 'Ipsilon project Contributors',
+    maintainer_email = 'ipsilon@lists.fedorahosted.org',
     url='https://fedorahosted.org/ipsilon/',
     packages = ['ipsilon', 'ipsilon.admin', 'ipsilon.login',
                 'ipsilon.info', 'ipsilon.util',
